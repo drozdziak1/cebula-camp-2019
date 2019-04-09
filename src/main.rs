e3a627ca780a6131e10868b45706a1a7cc14ed24
Stan Drozd <drozdziak1@gmail.com>
2019-04-09 15:06:53 +0200

    Write a small program demonstrating user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use rpassword::prompt_password_stdout;
+
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut username = String::new();
+    while username.is_empty() {
+        print!("Username: ");
+        io::stdout().flush().unwrap();
+
+        let stdin = io::stdin();
+        stdin.read_line(&mut username).unwrap();
+        username = username.trim().to_owned();
+    }
+
+    let mut password = String::new();
+    while password.is_empty() {
+        password = prompt_password_stdout("Password: ").unwrap();
+    }
+
+    println!(
+        "Hello {}, you entered a pass {} chars long",
+        username,
+        password.len()
+    );
 }
